c30014c197fa6861e324ac4b8183eae8d2a5abad
GonzaloDavid <davidgonzalomejia@hotmail.com>
2018-07-29 10:01:28 -0500

    Login con base de datos mysql

---
+++ ControlAccesoWeb/src/java/Controladores/CrearCuentaa.java
@@ -0,0 +1,105 @@
+package Controladores;
+
+import enviomail.javamail.Mail;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Gonzalo
+ */
+@ManagedBean
+@RequestScoped
+public class CrearCuentaa {
+
+    String nombre;
+    String apellido;
+    String cedula;
+    String correo;
+    String Password;
+    String confPassword;
+    conexion.Conectar conectar=new conexion.Conectar();
+    String validarConrreo;
+    String validarContrasenia;
+
+    public CrearCuentaa() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        System.out.println("EL valor de cedula es :" + cedula);
+        this.cedula = cedula;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+        
+    }
+
+    public String getConfPassword() {
+        return confPassword;
+    }
+
+    public void setConfPassword(String confPassword) {
+        this.confPassword = confPassword;
+    }
+
+    public void enviarMail() {
+        Mail mail = new Mail();
+        mail.sendEmail(this.correo, "Acceso al sistema", "Usuario: " + this.nombre+"\n" + "\nPassword: " + this.Password);
+
+    }
+    public void insertarUsuario()
+    {
+    enviarMail();
+    conectar.EjecutarSQL("Insert into artesano(nombre,apellido,correo,password1,confPassword)values('"+this.nombre+"','"+this.apellido+"','"+this.correo+"','"+this.Password+"','"+this.confPassword+"')");
+    }
+    public void comprobarCuenta()
+    {
+     
+     ResultSet consulta=conectar.Consulta("select *from artesano a where a.nombre='"+nombre+"'"+"&& a.password1='"+Password+"'");
+       try{
+            while(consulta.next()){
+                validarConrreo=consulta.getString(4);
+                validarContrasenia=consulta.getString(5);
+                System.out.println(validarConrreo);
+                 
+            }    
+        }catch(SQLException e){   
+            System.out.println("hay error en el while");
+        }  
+    }
+}
